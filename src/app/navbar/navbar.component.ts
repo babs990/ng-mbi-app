e34741b85283d512ee4da706b36d45c255c98663
babacar <nguiranebabacar305@gmail.com>
2024-06-04 23:28:37 +0000

    version 1

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component,ElementRef } from '@angular/core';
+import { Component,ElementRef, OnInit } from '@angular/core';
 import { DropDirective } from './drop.directive';
 
 @Component({
@@ -8,9 +8,13 @@ import { DropDirective } from './drop.directive';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
   constructor(private el : ElementRef){}
+
+  ngOnInit(): void {
+    this.el = this.el
+  }
 
   /**
    * masquer dropList
@@ -19,4 +23,38 @@ export class NavbarComponent {
     this.el.nativeElement.querySelector('#dropService').classList.add('hide')
     this.el.nativeElement.querySelector('#dropDevis').classList.add('hide')
   }
+
+  /**
+   * Afficher ou masquer la liste du menu
+   */
+  dropMenu(){
+  if(!!this.el.nativeElement.querySelector('#list').classList.contains('display')){
+    this.el.nativeElement.querySelector('#list').classList.remove('display')
+  }else{
+    this.el.nativeElement.querySelector('#list').classList.add('display')
+  }
+  }
+
+  /**
+   * Afficher ou masquer liste  services
+   */
+  dropService(){
+    if(!!this.el.nativeElement.querySelector('#lesServices').classList.contains('display')){
+      this.el.nativeElement.querySelector('#lesServices').classList.remove('display')
+    }else{
+      this.el.nativeElement.querySelector('#lesServices').classList.add('display')
+    }
+  }
+
+  /**
+   * Afficher ou masquer liste  Devis
+   */
+  dropDevis(){
+    if(!!this.el.nativeElement.querySelector('#lesDevis').classList.contains('display')){
+      this.el.nativeElement.querySelector('#lesDevis').classList.remove('display')
+    }else{
+      this.el.nativeElement.querySelector('#lesDevis').classList.add('display')
+    }
+  }
+
 }
