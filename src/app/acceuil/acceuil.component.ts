7f4887b44592240eaa82a857ce991ad83ea01afe
babacar <nguiranebabacar305@gmail.com>
2024-06-30 01:12:05 +0000

    version 2

--- src/app/acceuil/acceuil.component.ts
+++ src/app/acceuil/acceuil.component.ts
@@ -3,31 +3,30 @@ import { NavbarComponent } from "../navbar/navbar.component";
 import { choix } from './Choix';
 import { FooterComponent } from '../footer/footer.component';
 import { NgOptimizedImage } from '@angular/common';
+import { IntersectionDirectiveDirective } from './intersection-directive.directive';
 
 @Component({
     selector: 'app-acceuil',
     standalone: true,
     templateUrl: './acceuil.component.html',
     styleUrl: './acceuil.component.css',
-    imports: [NavbarComponent,FooterComponent,NgOptimizedImage]
+    imports: [NavbarComponent,FooterComponent,NgOptimizedImage,IntersectionDirectiveDirective]
 })
 export class AcceuilComponent {
 
     Choix = choix 
 
-    constructor(private el:ElementRef){
-
+    constructor(private el:ElementRef){}
+    /** Lancer la video */
+    playVideo(){
+        const play = this.el.nativeElement.querySelector('#video').querySelector('video')
+        const bouton = this.el.nativeElement.querySelector('#videoImg')
+        play.controls = true
+        bouton.style.display='none'
     }
 
-    /**
-     * Play
-    */
-   playVideo(){
-    const play = this.el.nativeElement.querySelector('#video').querySelector('video')
-    const bouton = this.el.nativeElement.querySelector('#videoImg')
-    play.controls = true
-    bouton.style.display='none'
-   }
-
-   /* Animation */
+    /** fixer nav */
+    navFixe() {
+        console.log('bonjour')
+    }        
 }
