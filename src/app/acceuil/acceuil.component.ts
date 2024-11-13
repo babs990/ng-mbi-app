70f32ca9a622909ad28e6dc2724331027f45f4bd
babacar <nguiranebabacar305@gmail.com>
2024-11-13 00:53:21 +0000

    modif image

--- src/app/acceuil/acceuil.component.ts
+++ src/app/acceuil/acceuil.component.ts
@@ -3,14 +3,13 @@ import { NavbarComponent } from "../navbar/navbar.component";
 import { choix } from './Choix';
 import { FooterComponent } from '../footer/footer.component';
 import { NgOptimizedImage } from '@angular/common';
-import { IntersectionDirectiveDirective } from './intersection-directive.directive';
 
 @Component({
     selector: 'app-acceuil',
     standalone: true,
     templateUrl: './acceuil.component.html',
     styleUrl: './acceuil.component.css',
-    imports: [NavbarComponent,FooterComponent,NgOptimizedImage,IntersectionDirectiveDirective]
+    imports: [NavbarComponent,FooterComponent,NgOptimizedImage]
 })
 export class AcceuilComponent {
 
